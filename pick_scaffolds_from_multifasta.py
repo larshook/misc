a954734cd82c1bed1bb0feb70bc0864b8ae4802f
Lars Höök <larsfredrikhook@hotmail.com>
2021-02-05 18:24:26 +0100

    Update pick_scaffolds_from_multifasta.py

--- pick_scaffolds_from_multifasta.py
+++ pick_scaffolds_from_multifasta.py
@@ -8,16 +8,16 @@
 from Bio import SeqIO
 import sys
 
-fasta_file = sys.argv[1]
-list_file = sys.argv[2]
-scaffold_file = sys.argv[3]
+FASTA_FILE = sys.argv[1]
+LIST_FILE = sys.argv[2]
+SCAFFOLD_FILE = sys.argv[3]
 
-with open(list_file) as id_handle:
+with open(LIST_FILE) as id_handle:
     wanted = set(line.rstrip("\n").split(None, 1)[0] for line in id_handle)
-print("Found %i unique identifiers in %s" % (len(wanted), list_file))
+print("Found %i unique identifiers in %s" % (len(wanted), LIST_FILE))
 
-records = (r for r in SeqIO.parse(fasta_file, "fasta") if r.id in wanted)
-count = SeqIO.write(records, scaffold_file, "fasta")
-print("Saved %i records from %s to %s" % (count, fasta_file, scaffold_file))
+records = (r for r in SeqIO.parse(FASTA_FILE, "fasta") if r.id in wanted)
+count = SeqIO.write(records, SCAFFOLD_FILE, "fasta")
+print("Saved %i records from %s to %s" % (count, FASTA_FILE, SCAFFOLD_FILE))
 if count < len(wanted):
-    print("Warning %i IDs not found in %s" % (len(wanted) - count, fasta_file))
+    print("Warning %i IDs not found in %s" % (len(wanted) - count, FASTA_FILE))
